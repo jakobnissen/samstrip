4e9abc3524c04d65ba8e92827bb0d1adff9c85fe
Jakob Nybo Nissen <jakobnybonissen@gmail.com>
2024-11-26 13:15:17 +0100

    Add some tests

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,36 @@ use clap::Parser;
 use core::str;
 use std::io::{self, BufRead, BufReader, BufWriter, Write};
 
+#[derive(Debug)]
+enum SamError {
+    TooFewFields,
+    MissingHeader,
+    BadAuxTag,
+}
+
+impl SamError {
+    fn exit(self) {
+        match self {
+            Self::TooFewFields => {
+                eprintln!("Error: In SAM alignment line, did not see all required fields:");
+            }
+            Self::MissingHeader => {
+                eprintln!(
+                    "Error: First SAM line did not start with a @, indicating a missing header. \
+                    \nDid you remember to pass in the whole SAM file? \
+                    If producing the SAM file using `samtools view`, remember the `-h` flag to include the header."
+                );
+            }
+            Self::BadAuxTag => {
+                eprintln!(
+                    "Error: Could not parse SAM auxiliary field: Less than two bytes in length"
+                );
+            }
+        };
+        std::process::exit(1)
+    }
+}
+
 // Iterates over tab-separated fields in u8 slice. This uses memchr for efficiency.
 struct FieldIterator<'a> {
     // None if the iterator has no more elements to output,
@@ -89,7 +119,7 @@ impl Tags {
         }
     }
 
-    fn add_fields<W: Write>(&self, io: &mut W, line: &[u8]) {
+    fn add_fields<W: Write>(&self, io: &mut W, line: &[u8]) -> Result<(), SamError> {
         let mut need_newline = true;
         match self {
             Self::RemoveAll => (),
@@ -99,24 +129,59 @@ impl Tags {
                 need_newline = false;
             }
             Self::Keep(k) => {
-                for mem in FieldIterator::new(line).filter(|m| starts_with_tag(m, k)) {
-                    unwrap_pipe(io.write_all(b"\t"));
-                    unwrap_pipe(io.write_all(mem));
-                    need_newline = mem.last().map_or(false, |&b| b != b'\n');
+                for mem in FieldIterator::new(line) {
+                    if starts_with_tag(mem, k)? {
+                        unwrap_pipe(io.write_all(b"\t"));
+                        unwrap_pipe(io.write_all(mem));
+                        need_newline = mem.last().map_or(false, |&b| b != b'\n');
+                    }
                 }
             }
             Self::Remove(r) => {
-                for mem in FieldIterator::new(line).filter(|m| !starts_with_tag(m, r)) {
-                    unwrap_pipe(io.write_all(b"\t"));
-                    unwrap_pipe(io.write_all(mem));
-                    need_newline = mem.last().map_or(false, |&b| b != b'\n');
+                for mem in FieldIterator::new(line) {
+                    if !starts_with_tag(mem, r)? {
+                        unwrap_pipe(io.write_all(b"\t"));
+                        unwrap_pipe(io.write_all(mem));
+                        need_newline = mem.last().map_or(false, |&b| b != b'\n');
+                    }
                 }
             }
-        }
+        };
         if need_newline {
             unwrap_pipe(io.write_all(b"\n"));
         }
+        Ok(())
     }
+}
+
+fn samstrip<I: BufRead, W: Write>(
+    inp: &mut I,
+    out: &mut W,
+    tags: &Tags,
+    noheader: bool,
+) -> Result<(), SamError> {
+    let mut line = write_header(out, inp, noheader)?;
+    while !line.is_empty() {
+        // Write the line from the start to the 9th tab, as the first 9 fields
+        // should be copied over unchanged
+        let ninth = memchr::memchr_iter(b'\t', &line)
+            .nth(8)
+            .ok_or(SamError::TooFewFields)?;
+        unwrap_pipe(out.write_all(&line[..=ninth]));
+        // The 10th and 11th field are SEQ and QUAL - we replace them with *
+        unwrap_pipe(out.write_all(b"*\t*"));
+        if let Some(aux_mem) = memchr::memchr_iter(b'\t', &line[ninth + 1..])
+            .nth(1)
+            .map(|eleventh| &line[eleventh + ninth + 2..])
+        {
+            tags.add_fields(out, aux_mem)?;
+        } else {
+            unwrap_pipe(out.write_all(b"\n"));
+        }
+        line.clear();
+        inp.read_until(b'\n', &mut line).unwrap();
+    }
+    Ok(())
 }
 
 fn main() {
@@ -126,44 +191,23 @@ fn main() {
     // a small speedup
     let mut stdin = BufReader::new(io::stdin().lock());
     let mut stdout = BufWriter::new(io::stdout().lock());
-    let mut line = write_header(&mut stdout, &mut stdin, cli.noheader);
-    while !line.is_empty() {
-        // Write the line from the start to the 9th tab, as the first 9 fields
-        // should be copied over unchanged
-        let ninth = memchr::memchr_iter(b'\t', &line).nth(8).unwrap_or_else(|| {
-            eprintln!("Error: In SAM alignment line, did not see all required fields");
-            std::process::exit(1)
-        });
-        unwrap_pipe(stdout.write_all(&line[..=ninth]));
-        // The 10th and 11th field are SEQ and QUAL - we replace them with *
-        unwrap_pipe(stdout.write_all(b"*\t*"));
-        if let Some(aux_mem) = memchr::memchr_iter(b'\t', &line[ninth + 1..])
-            .nth(1)
-            .map(|eleventh| &line[eleventh + ninth + 2..])
-        {
-            tags.add_fields(&mut stdout, aux_mem);
-        } else {
-            unwrap_pipe(stdout.write_all(b"\n"));
-        }
-        line.clear();
-        stdin.read_until(b'\n', &mut line).unwrap();
-    }
+    samstrip(&mut stdin, &mut stdout, &tags, cli.noheader).unwrap_or_else(SamError::exit)
 }
 
 // Returns the first non-header line (or an empty Vec, if no such line)
-fn write_header<O: Write, I: BufRead>(out: &mut O, inp: &mut I, noheader: bool) -> Vec<u8> {
+fn write_header<O: Write, I: BufRead>(
+    out: &mut O,
+    inp: &mut I,
+    noheader: bool,
+) -> Result<Vec<u8>, SamError> {
     let first_byte = inp.fill_buf().unwrap().first().copied();
     // Unless --noheader is passed, error if the first byte exists but is not a @
     if !noheader && first_byte.is_some_and(|b| b != b'@') {
-        eprintln!(
-        "Error: First SAM line did not start with a @, indicating a missing header. \
-        \nDid you remember to pass in the whole SAM file? \
-        If producing the SAM file using `samtools view`, remember the `-h` flag to include the header."
-    );
-        std::process::exit(1);
+        return Err(SamError::MissingHeader);
     }
     let mut line: Vec<u8> = Vec::new();
     let mut seen_versions: Vec<u32> = Vec::new();
+    let mut needs_newline = false;
     while inp.read_until(b'\n', &mut line).unwrap() > 0 {
         if !line.starts_with(b"@") {
             break;
@@ -172,7 +216,11 @@ fn write_header<O: Write, I: BufRead>(out: &mut O, inp: &mut I, noheader: bool) 
         // where X is some u32. Since it must be unique, we need to keep track of what versions
         // we've already seen in the file, so we can pick the first version not in the file
         if line.starts_with(b"@PG\t") {
-            if let Some(ver) = FieldIterator::new(&line)
+            let stripped = line
+                .strip_suffix(b"\r\n")
+                .or(line.strip_suffix(b"\n"))
+                .unwrap_or(&line);
+            if let Some(ver) = FieldIterator::new(stripped)
                 .find(|mem| mem.starts_with(b"ID:samstrip."))
                 .and_then(|mem| str::from_utf8(&mem[b"ID:samstrip.".len()..]).ok())
                 .and_then(|s| s.parse::<u32>().ok())
@@ -181,6 +229,7 @@ fn write_header<O: Write, I: BufRead>(out: &mut O, inp: &mut I, noheader: bool) 
             }
         }
         unwrap_pipe(out.write_all(&line));
+        needs_newline = line.last().map(|&b| b != b'\n').unwrap_or(false);
         line.clear();
     }
     // The idea here is that after sorting and dedupping, seen_versions must correspond contain the
@@ -198,9 +247,12 @@ fn write_header<O: Write, I: BufRead>(out: &mut O, inp: &mut I, noheader: bool) 
         .position(|(i, &v)| i as u32 != v)
         .unwrap_or(seen_versions.len()) as u32;
     if first_byte.is_some_and(|b| b == b'@') {
+        if needs_newline {
+            unwrap_pipe(out.write_all(b"\n"));
+        }
         write_pg_header(out, first_unseen_version);
     }
-    line
+    Ok(line)
 }
 
 // This is useful for consumers of the stripped SAM file, because they probably wonder
@@ -236,10 +288,9 @@ fn convert_tags(tags: Vec<String>) -> Vec<u16> {
     }).collect()
 }
 
-fn starts_with_tag(mem: &[u8], tags: &[u16]) -> bool {
+fn starts_with_tag(mem: &[u8], tags: &[u16]) -> Result<bool, SamError> {
     if mem.len() < 2 {
-        eprintln!("Error: Could not parse SAM auxiliary field: Less than two bytes in length");
-        std::process::exit(1)
+        return Err(SamError::BadAuxTag);
     }
     // Safety: We read first two elements and just checked there are at least two elements.
     // Note that this works correctly for both big- and little endian platforms,
@@ -251,7 +302,7 @@ fn starts_with_tag(mem: &[u8], tags: &[u16]) -> bool {
     // SIMD'ing does not degrade performance measurably, because the extra branches related to whether
     // the SIMD loop should kick in is constant across the program's run, and therefore will be almost
     // perfectly predicted by the branch predictor.
-    tags.iter().fold(false, |acc, &t| acc | (t == u))
+    Ok(tags.iter().fold(false, |acc, &t| acc | (t == u)))
 }
 
 // This is necessary because stdout may decide to stop allowing more input. E.g. if
@@ -272,10 +323,207 @@ fn unwrap_pipe<T>(x: std::io::Result<T>) -> T {
     }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const LINES: &'static str = "@HD\tVN:1
+@SQ\tSN:S1\tLN:5000
+@SQ\tSN:S2\tLN:9000
+R1\t163\tS1\t1\t0\t3S147M\t=\t158\t307\tTAGA\tIIII\tNM:i:0\tms:i:294\tAS:i:294\tnn:i:0\ttp:A:P\tcm:i:20\ts1:i:271\ts2:i:0\tde:f:0\trl:i:0
+R2\t163\tS1\t1\t0\t34S116M\t=\t71\t220\tTAGA\tIIII\tNM:i:0\tms:i:232\tAS:i:232\tnn:i:0\ttp:A:P\tcm:i:15\ts1:i:206\ts2:i:206\tde:f:0\trl:i:0
+R3\t101\tS2\t1\t0\t*\t=\t1\t0\tTAGA\tIIII\trl:i:0
+R4\t101\tS2\t1\t0\t*\t=\t1\t0\tTAGA\tIIII\trl:i:0
+R5\t163\tS3\t1\t0\t36S114M\t=\t69\t218\tTAGA\tIIII\tNM:i:0\tms:i:228\tAS:i:228\tnn:i:0\ttp:A:P\tcm:i:15\ts1:i:205\ts2:i:0\tde:f:0\trl:i:0";
+
+    fn strip(inp: &str, tags: &Tags, noheader: bool) -> Result<String, SamError> {
+        let mut out: Vec<u8> = Vec::new();
+        samstrip(&mut inp.as_bytes(), &mut out, &tags, noheader)?;
+        let s = String::from_utf8(out).unwrap();
+        // Remove the CL field in PG header, as that contains arbitrary
+        // data when `cargo test` is run
+        Ok(s.lines()
+            .map(|line| {
+                if line.starts_with("@PG") {
+                    line.split('\t')
+                        .map(|field| {
+                            if field.starts_with("CL:") {
+                                "CL:placeholder"
+                            } else {
+                                field
+                            }
+                        })
+                        .collect::<Vec<_>>()
+                        .join("\t")
+                } else {
+                    line.to_owned()
+                }
+            })
+            .collect::<Vec<_>>()
+            .join("\n"))
+    }
+
+    fn strip_default(inp: &str) -> Result<String, SamError> {
+        strip(inp, &Tags::new(None, None), false)
+    }
+
+    #[test]
+    fn normal_run() {
+        let inp = strip_default(LINES).unwrap();
+        assert_eq!(
+            inp,
+            "@HD\tVN:1
+@SQ\tSN:S1\tLN:5000
+@SQ\tSN:S2\tLN:9000
+@PG\tID:samstrip.0\tPN:samstrip\tVN:0.2.1\tCL:placeholder\tDS:See https://github.com/jakobnissen/samstrip
+R1\t163\tS1\t1\t0\t3S147M\t=\t158\t307\t*\t*\tNM:i:0
+R2\t163\tS1\t1\t0\t34S116M\t=\t71\t220\t*\t*\tNM:i:0
+R3\t101\tS2\t1\t0\t*\t=\t1\t0\t*\t*
+R4\t101\tS2\t1\t0\t*\t=\t1\t0\t*\t*
+R5\t163\tS3\t1\t0\t36S114M\t=\t69\t218\t*\t*\tNM:i:0"
+        );
+    }
+
+    #[test]
+    fn increments_pg() {
+        let mut inp = "@PG\tID:samstrip.0\n".to_owned();
+        let hdr = LINES
+            .lines()
+            .filter(|s| s.starts_with('@'))
+            .collect::<Vec<_>>()
+            .join("\n");
+        inp.push_str(&hdr);
+        assert_eq!(strip_default(&inp).unwrap(), "@PG\tID:samstrip.0
+@HD\tVN:1
+@SQ\tSN:S1\tLN:5000
+@SQ\tSN:S2\tLN:9000
+@PG\tID:samstrip.1\tPN:samstrip\tVN:0.2.1\tCL:placeholder\tDS:See https://github.com/jakobnissen/samstrip")
+    }
+
+    #[test]
+    fn missing_pg() {
+        let mut inp = "@PG\tID:samstrip.0\n@PG\tID:samstrip.2\n".to_owned();
+        let hdr = LINES
+            .lines()
+            .filter(|s| s.starts_with('@'))
+            .collect::<Vec<_>>()
+            .join("\n");
+        inp.push_str(&hdr);
+        assert_eq!(strip_default(&inp).unwrap(), "@PG\tID:samstrip.0\n@PG\tID:samstrip.2
+@HD\tVN:1
+@SQ\tSN:S1\tLN:5000
+@SQ\tSN:S2\tLN:9000
+@PG\tID:samstrip.1\tPN:samstrip\tVN:0.2.1\tCL:placeholder\tDS:See https://github.com/jakobnissen/samstrip")
+    }
+
+    #[test]
+    fn noheader() {
+        let res = strip(
+            "R3\t101\tS2\t1\t0\t*\t=\t1\t0\tTAGA\tIIII\trl:i:0",
+            &Tags::new(None, None),
+            true,
+        )
+        .unwrap();
+        assert_eq!(res, "R3\t101\tS2\t1\t0\t*\t=\t1\t0\t*\t*");
+    }
+
+    #[test]
+    fn test_noheader() {
+        let res = strip(
+            "R3\t101\tS2\t1\t0\t*\t=\t1\t0\tTAGA\tIIII\trl:i:0",
+            &Tags::new(None, None),
+            false,
+        );
+        assert!(matches!(res, Err(SamError::MissingHeader)));
+    }
+
+    #[test]
+    fn test_keep_none() {
+        let res = strip(LINES, &Tags::new(Some(vec![]), None), false).unwrap();
+        assert_eq!(
+            res,
+            "@HD\tVN:1
+@SQ\tSN:S1\tLN:5000
+@SQ\tSN:S2\tLN:9000
+@PG\tID:samstrip.0\tPN:samstrip\tVN:0.2.1\tCL:placeholder\tDS:See https://github.com/jakobnissen/samstrip
+R1\t163\tS1\t1\t0\t3S147M\t=\t158\t307\t*\t*
+R2\t163\tS1\t1\t0\t34S116M\t=\t71\t220\t*\t*
+R3\t101\tS2\t1\t0\t*\t=\t1\t0\t*\t*
+R4\t101\tS2\t1\t0\t*\t=\t1\t0\t*\t*
+R5\t163\tS3\t1\t0\t36S114M\t=\t69\t218\t*\t*"
+        );
+    }
+
+    #[test]
+    fn test_remove_none() {
+        let res = strip(LINES, &Tags::new(None, Some(vec![])), false).unwrap();
+        assert_eq!(
+            res,
+            "@HD\tVN:1
+@SQ\tSN:S1\tLN:5000
+@SQ\tSN:S2\tLN:9000
+@PG\tID:samstrip.0\tPN:samstrip\tVN:0.2.1\tCL:placeholder\tDS:See https://github.com/jakobnissen/samstrip
+R1\t163\tS1\t1\t0\t3S147M\t=\t158\t307\t*\t*\tNM:i:0\tms:i:294\tAS:i:294\tnn:i:0\ttp:A:P\tcm:i:20\ts1:i:271\ts2:i:0\tde:f:0\trl:i:0
+R2\t163\tS1\t1\t0\t34S116M\t=\t71\t220\t*\t*\tNM:i:0\tms:i:232\tAS:i:232\tnn:i:0\ttp:A:P\tcm:i:15\ts1:i:206\ts2:i:206\tde:f:0\trl:i:0
+R3\t101\tS2\t1\t0\t*\t=\t1\t0\t*\t*\trl:i:0
+R4\t101\tS2\t1\t0\t*\t=\t1\t0\t*\t*\trl:i:0
+R5\t163\tS3\t1\t0\t36S114M\t=\t69\t218\t*\t*\tNM:i:0\tms:i:228\tAS:i:228\tnn:i:0\ttp:A:P\tcm:i:15\ts1:i:205\ts2:i:0\tde:f:0\trl:i:0"
+        );
+    }
+
+    #[test]
+    fn test_keep_rl() {
+        let res = strip(LINES, &Tags::new(Some(vec!["rl".to_owned()]), None), false).unwrap();
+        assert_eq!(
+            res,
+            "@HD\tVN:1
+@SQ\tSN:S1\tLN:5000
+@SQ\tSN:S2\tLN:9000
+@PG\tID:samstrip.0\tPN:samstrip\tVN:0.2.1\tCL:placeholder\tDS:See https://github.com/jakobnissen/samstrip
+R1\t163\tS1\t1\t0\t3S147M\t=\t158\t307\t*\t*\trl:i:0
+R2\t163\tS1\t1\t0\t34S116M\t=\t71\t220\t*\t*\trl:i:0
+R3\t101\tS2\t1\t0\t*\t=\t1\t0\t*\t*\trl:i:0
+R4\t101\tS2\t1\t0\t*\t=\t1\t0\t*\t*\trl:i:0
+R5\t163\tS3\t1\t0\t36S114M\t=\t69\t218\t*\t*\trl:i:0"
+        );
+    }
+
+    #[test]
+    fn test_remove_some() {
+        let res = strip(
+            LINES,
+            &Tags::new(
+                None,
+                Some(
+                    vec!["rl", "s1", "cm"]
+                        .iter()
+                        .map(|s| s.to_string())
+                        .collect(),
+                ),
+            ),
+            false,
+        )
+        .unwrap();
+        assert_eq!(
+            res,
+            "@HD\tVN:1
+@SQ\tSN:S1\tLN:5000
+@SQ\tSN:S2\tLN:9000
+@PG\tID:samstrip.0\tPN:samstrip\tVN:0.2.1\tCL:placeholder\tDS:See https://github.com/jakobnissen/samstrip
+R1\t163\tS1\t1\t0\t3S147M\t=\t158\t307\t*\t*\tNM:i:0\tms:i:294\tAS:i:294\tnn:i:0\ttp:A:P\ts2:i:0\tde:f:0
+R2\t163\tS1\t1\t0\t34S116M\t=\t71\t220\t*\t*\tNM:i:0\tms:i:232\tAS:i:232\tnn:i:0\ttp:A:P\ts2:i:206\tde:f:0
+R3\t101\tS2\t1\t0\t*\t=\t1\t0\t*\t*
+R4\t101\tS2\t1\t0\t*\t=\t1\t0\t*\t*
+R5\t163\tS3\t1\t0\t36S114M\t=\t69\t218\t*\t*\tNM:i:0\tms:i:228\tAS:i:228\tnn:i:0\ttp:A:P\ts2:i:0\tde:f:0"
+        );
+    }
+}
+
 const HELP_MESSAGE: &str =
     "Reads a SAM file from stdin, and prints the equivalent stripped file to stdout.
-A stripped file has the SEQ and QUAL fields removed, and all auxiliary fields.
-Barring any aligner-specific auxiliary fields, a stripped SAM file contain the same
+A stripped file has the SEQ and QUAL fields removed, and auxiliary tags depending
+on the setting.
+Barring any aligner-specific auxiliary tags, a stripped SAM file contain the same
 alignment information as a full file, but takes up less disk space.
 
 The program optionally takes `--keep`, a list of auxiliary tags to keep in the output.
@@ -286,7 +534,7 @@ Examples:
 `cat in | samstrip --keep > out` - do not keep any tags
 
 Similarly, the `--remove` option only removes the given tags. If no tags are passed,
-all tags are kept.
+to `--remove`, all tags are kept.
 
 Example usage:
 Stripping an exiting BAM file:
